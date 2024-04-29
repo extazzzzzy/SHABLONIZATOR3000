bd18af6ff8826f789e119cd14159cf571734ce4b
danyakovalugrasu <danyakovalugrasu@gmail.com>
2024-04-29 13:37:07 +0500

    подключение питона и запуск через пхп

--- python/main.py
+++ python/main.py
@@ -3,7 +3,7 @@ import sys
 
 fullname = sys.argv[1]
 address = sys.argv[2]
-doc = DocxTemplate("template.docx")
+doc = DocxTemplate("../templates/template.docx")
 context = {"fullname": fullname, "address": address}
 doc.render(context)
-doc.save("test.docx")
+doc.save("../documents/test.docx")
