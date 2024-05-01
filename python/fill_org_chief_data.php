f6e14740bf671ec8a4537fa3ac3572d229498275
danyakovalugrasu <danyakovalugrasu@gmail.com>
2024-05-01 20:50:20 +0500

    Сделал скрипт для заполнения данных на этапе  руководителя практики от организации

--- python/fill_org_chief_data.php
+++ python/fill_org_chief_data.php
@@ -30,4 +30,7 @@ $connectMySQL = new mysqli('localhost', 'root', 'root', 'shablonizator3000');
 $connectMySQL->query("UPDATE `diary_document` SET `STUDENT_QUALITIES` = '$student_qualities', `PROBLEM_SOLVING_SPEED` = '$problem_solving_speed', `WORK_AMOUNT` = '$work_amount', `WORK_YEAR` = '$work_year', `REMARKS` = '$remarks', `STUDENT_ASSESSMENT` = '12', `PRACTICE_PLACE` = '$practice_place', `PRACTICE_PLACE_ADDRESS` = '$practice_place_address', `PRACTICE_DEADLINES` = '$practice_deadlines' WHERE `ID` = $diary_document_id");
 
 $src = $connectMySQL->query("SELECT SRC FROM `diary_document` WHERE `id` = '$diary_document_id'")->fetch_assoc()['SRC'];
+
+// внесение недостающих данных в документ через питон и повторное сохранение уже существующего документа
+$result = shell_exec('python org_chief_update_document.py ' . escapeshellarg($src) . ' ' .  escapeshellarg($practice_place) . ' ' . escapeshellarg($practice_place_address) . ' ' . escapeshellarg($work_year) . ' ' . escapeshellarg($practice_deadlines) . ' ' . escapeshellarg($student_qualities) . ' ' . escapeshellarg($problem_solving_speed) . ' ' . escapeshellarg($work_amount) . ' ' . escapeshellarg($remarks) . ' ' . escapeshellarg($student_assessment));
 ?>
