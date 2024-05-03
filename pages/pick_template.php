dbffcf9ff2fb63a2b5502f4096f7fd58128d0db4
h-idwidwi <hidwdiwi@gmail.com>
2024-05-03 15:24:32 +0500

    Добавлено две цифры

--- pages/pick_template.php
+++ pages/pick_template.php
@@ -41,6 +41,8 @@ ob_start(); // начало буферизации вывода
             <option value="">Выберите неделю практики</option>
             <option value="1">1</option>
             <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
         </select>
     </div>
     <input type="submit" name="submit" value="Отправить документы">
