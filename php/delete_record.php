e740b4942f25b762c4a4beba5f6281a20a8bb356
17Vector <131187562+17Vector@users.noreply.github.com>
2024-05-03 19:30:50 +0500

    Update delete_record.php

--- php/delete_record.php
+++ php/delete_record.php
@@ -4,12 +4,20 @@
     $doc_id = $_POST['doc_id'];
 
     $connectMySQL = new mysqli('localhost', 'root', 'root', 'shablonizator3000');
-    $sql = "DELETE FROM diary_document WHERE ID = '$doc_id'";
+
+    $sql = "DELETE FROM user_to_document_to_agreement WHERE DOCUMENT_ID = '$doc_id'";
 
     if ($connectMySQL->query($sql) === TRUE) {
-        header('Location: ../pages/documents.php');
-    } else {
-        echo "Ошибка при удалении документа: " . $mysql->error;
+        $sql = "DELETE FROM diary_document WHERE ID = '$doc_id'";
+
+        if ($connectMySQL->query($sql) === TRUE) {
+            header('Location: ../pages/documents.php');
+        } else {
+            echo "Ошибка при удалении документа: " . $connectMySQL->error;
+        }
+    } 
+    else {
+        echo "Ошибка при удалении документа: " . $connectMySQL->error;
     }
 
     $connectMySQL->close();
